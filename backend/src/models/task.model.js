9786d1803c946e708f52f237b4dd7093c39b006f
KeinPC <kein.carrillo.escom@gmail.com>
2025-08-29 23:03:09 -0600

    db: 🗃️ modelo de task

---
+++ backend/src/models/task.model.js
@@ -0,0 +1,26 @@
+import sequelize from '../db'
+import { DataTypes } from 'sequelize'
+
+const Task = sequelize.define('task', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+})
+
+export default Task
