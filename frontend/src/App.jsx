089269791fbaef1fd3c76a17e35ead5190512ea4
KeinPC <kein.carrillo.escom@gmail.com>
2025-08-30 18:56:21 -0600

    feat: ✨ añadir rutas con react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/signup" element={<h1>Signup</h1>} />
+        <Route path="/signin" element={<h1>Signin</h1>} />
+        <Route path="/tasks" element={<h1>Tasks</h1>} />
+        <Route path="/add-tasks" element={<h1>Add-tasks</h1>} />
+        <Route path="/task/:id" element={<h1>Task id</h1>} />
+        <Route path="/profile" element={<h1>profile</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
