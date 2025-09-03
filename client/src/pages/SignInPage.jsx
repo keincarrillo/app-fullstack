58d8be73da6cc5eaa1d09a9e87398ad8e929a3f6
KeinPC <kein.carrillo.escom@gmail.com>
2025-09-02 21:49:32 -0600

    feat: ✨ añadir link a sinin o sinup

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -1,15 +1,28 @@
 import { useForm } from 'react-hook-form'
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { AuthContext } from '../context/AuthContext'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const SignInPage = () => {
-  const { signin, errors: signInErrors } = useContext(AuthContext)
+  const {
+    signin,
+    errors: signInErrors,
+    isAuthenticated
+  } = useContext(AuthContext)
   const {
     register,
     handleSubmit,
     formState: { errors }
   } = useForm()
-  const onSubmit = handleSubmit(data => signin(data))
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isAuthenticated) navigate('/tasks')
+  }, [isAuthenticated])
+
+  const onSubmit = handleSubmit(async data => await signin(data))
 
   return (
     <div className="flex items-center justify-center h-screen bg-zinc-900">
@@ -19,7 +32,7 @@ const SignInPage = () => {
             className="bg-red-500 p-2 text-white text-center my-2 rounded-md"
             key={i}
           >
-            {error.message || error}{' '}
+            {error.message || error}
           </div>
         ))}
         <h1 className="text-2xl font-bold mb-4 text-white">Sign In</h1>
@@ -45,10 +58,19 @@ const SignInPage = () => {
           )}
           <button
             type="submit"
-            className="bg-sky-500 text-white px-4 py-2 rounded-md my-2"
+            className="bg-sky-500 text-white px-4 py-2 rounded-md my-2 hover:cursor-pointer hover:bg-sky-600"
           >
             Register
           </button>
+          <p>
+            Don't have an account?{' '}
+            <Link
+              to="/signup"
+              className="text-sky-500 hover:cursor-pointer hover:underline"
+            >
+              Sign Up
+            </Link>
+          </p>
         </form>
       </div>
     </div>
--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,7 +1,7 @@
 import { useForm } from 'react-hook-form'
 import { useContext, useEffect } from 'react'
 import { AuthContext } from '../context/AuthContext'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUpPage = () => {
   const {
@@ -32,10 +32,10 @@ const SignUpPage = () => {
             className="bg-red-500 p-2 text-white text-center my-2 rounded-md"
             key={i}
           >
-            {error}
+            {error.message || error}
           </div>
         ))}
-        <h1 className="text-2xl font-bold mb-4 text-white">Register</h1>
+        <h1 className="text-2xl font-bold mb-4 text-white">Sign Up</h1>
         <form onSubmit={onSubmit} className="flex flex-col">
           <input
             type="text"
@@ -67,10 +67,19 @@ const SignUpPage = () => {
 
           <button
             type="submit"
-            className="bg-sky-500 text-white px-4 py-2 rounded-md my-2"
+            className="bg-sky-500 text-white px-4 py-2 rounded-md my-2 hover:cursor-pointer hover:bg-sky-600"
           >
             Register
           </button>
+          <p>
+            Already have an account?{' '}
+            <Link
+              to="/signin"
+              className="text-sky-500 hover:cursor-pointer hover:underline"
+            >
+              Sign In
+            </Link>
+          </p>
         </form>
       </div>
     </div>
